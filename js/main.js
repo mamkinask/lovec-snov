fc0cf155890ff385011c9f46ea651224c5e82f4c
mamkinask <113915983+mamkinask@users.noreply.github.com>
2023-03-31 17:15:59 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+  console.log( "ready!" );
+});
+
+$(".oblakorooz").click(function(){
+  $(".ikran2").addClass("onosamoe");
+  $(".ikran1").addClass("netosamoe");
+});
